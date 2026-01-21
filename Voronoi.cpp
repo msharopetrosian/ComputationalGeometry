6792c6c0957cbe45ea5e4a1455025d4f2303dd19
msharopetrosian <sharopetrosyanmane@gmail.com>
2026-01-22 00:39:38 +0400

    Create Voronoi.cpp

---
+++ Voronoi.cpp
@@ -0,0 +1,10 @@
+#include "Voronoi.h"
+
+namespace cg {
+
+    // API-level Voronoi placeholder
+    std::vector<VoronoiEdge> voronoiDiagram(const std::vector<Point>&) {
+        return {};
+    }
+
+}
