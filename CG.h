740e1cd1b725a6fb9a8aaf938d2dd48956073d7e
msharopetrosian <sharopetrosyanmane@gmail.com>
2026-01-22 00:40:08 +0400

    Add CG.h header file with includes

---
+++ CG.h
@@ -0,0 +1,19 @@
+#ifndef CG_API_H
+#define CG_API_H
+
+#include "Point.h"
+#include "Vector.h"
+#include "Segment.h"
+#include "Ray.h"
+#include "Line.h"
+#include "Polygon.h"
+
+#include "Orientation.h"
+#include "Intersection.h"
+
+#include "Transform2D.h"
+
+#include "Delaunay.h"
+#include "Voronoi.h"
+
+#endif
